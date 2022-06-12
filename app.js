d21b359d790fe7a53130239903f69d3cbb9b07f6
Quoc Do <quocboy159@gmail.com>
2022-06-12 18:20:50 +0700

    seperate app api and server host

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./src/routes/todos');
-const usersRouter = require('./src/routes/users.routes').default;
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import bodyparser from 'body-parser'
+import compression from 'compression';
+import cors from 'cors'
+import { connectDb } from './src/helpers/db.helpers'
+import { generateRouters } from './src/routes'
 
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
+app.use(cors());
 
-app.use(indexRouter);
-app.use(usersRouter);
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+generateRouters(app);
+
+connectDb();
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(err.status || 404).json({
     message: "No such route exists"
   })
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
