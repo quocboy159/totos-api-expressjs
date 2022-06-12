adc6ae011be612f49bf80a630b885e0c5265fd1d
Quoc Do <quocboy159@gmail.com>
2022-06-12 20:40:49 +0700

    add unit test chai mocha supertest

---
+++ src/controllers/users.controllers.test.js
@@ -0,0 +1,53 @@
+import { describe } from "mocha";
+import request from 'supertest'
+import express from 'express';
+import { expect } from "chai";
+
+const app = express();
+const users = [
+    {
+        id: 1,
+        email: 'test100@gmail.com',
+        password: '1234567',
+        name: 'test 2',
+        phoneNumber: '1234567'
+    }
+];
+
+describe('users controllers', async => {
+    beforeEach(() => {
+        app.get('/users', (req, res) => {
+            res.status(200).json(users);
+        });
+
+        app.get('/users/:id', (req, res) => {
+            const user = users.find(x => x.id === +req.params.id);
+            res.status(200).json(user);
+        });
+    });
+
+    it('getAll successful', async () => {
+        const { status, body } = await request(app).get('/users');
+
+        expect(status).to.equal(200);
+        expect(users.length).to.equal(body.length);
+    });
+
+    it('getUserById successful', async () => {
+        const id = 1;
+        const { status, body } = await request(app).get(`/users/${id}`);
+        const user = users.find(x => x.id === id);
+
+        expect(status).to.equal(200);
+        expect(user).to.deep.equal(body);
+    });
+
+    it('getUserById returns null', async () => {
+        const id = Math.random();
+        const { status, body } = await request(app).get(`/users/${id}`);
+        const user = users.find(x => x.id === id);
+
+        expect(status).to.equal(200);
+        expect(user).to.deep.equal(undefined);
+    });
+});
--- src/helpers/initialize-db.helpers.js
+++ src/helpers/initialize-db.helpers.js
@@ -2,18 +2,15 @@
 import mongoose from 'mongoose'
 import userEntity from '../models/user'
 import { connectDb } from './db.helpers';
-import dotenv from 'dotenv'
 
 const initDb = async () => {
-    dotenv.config({ path: '../.env' })
-    console.log(process.env.MONGO_DB)
     await connectDb();
     await userEntity.insertMany([{
         _id: new mongoose.Types.ObjectId(),
-        email: 'test2@gmail.com',
+        email: 'test100@gmail.com',
         password: '1234567',
         name: 'test 2',
-        phone_number: '1234567'
+        phoneNumber: '1234567'
     }])
 }
 
